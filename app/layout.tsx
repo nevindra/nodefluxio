c449215c64a3f4fe31a0f937c7e70ef56bfaa9ec
nevindra <nevindraibnazhifi@gmail.com>
2025-01-22 10:22:09 +0700

    fix layout add more metadata

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,23 +1,28 @@
-import { generateMetadata } from "@/app/config/sharedMetadata";
-import Footer from "@/components/Footer";
 import NavigationBar from "@/components/Navbar";
+import Footer from "@/components/Footer";
+import { generateMetadata } from "@/app/config/sharedMetadata";
 import type { Metadata } from "next";
 import { Montserrat } from "next/font/google";
 import "./globals.css";
 
 const inter = Montserrat({ subsets: ["latin"] });
 
-export const metadata: Metadata = generateMetadata(
-  "Nodeflux",
-  "Nodeflux is an Indonesian artificial intelligence company specializing in vision AI, providing solutions for various industries and government sectors, and recognized internationally for its technological advancements and security certifications.",
-  [
-    "Vision AI",
-    "Computer Vision",
-    "Artificial Intelligence",
-    "Indonesian AI",
-    "AI Solutions",
-  ]
-);
+export const metadata: Metadata = {
+  ...generateMetadata(
+    "Nodeflux",
+    "Nodeflux is an Indonesian artificial intelligence company specializing in vision AI, providing solutions for various industries and government sectors, and recognized internationally for its technological advancements and security certifications.",
+    [
+      "Vision AI",
+      "Computer Vision",
+      "Artificial Intelligence",
+      "Indonesian AI",
+      "AI Solutions",
+    ]
+  ),
+  alternates: {
+    canonical: "https://www.nodeflux.io/",
+  },
+};
 
 export default function RootLayout({
   children,
@@ -26,16 +31,30 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      {/* Nevin */}
-      <meta
-        name="google-site-verification"
-        content="YJevxjbOP3qnYa2wMu3sQaDoy7cxLdH3cdTnDwbcqQI"
-      />
-      {/* Meidy */}
-      <meta
-        name="google-site-verification"
-        content="8SnxIePEB-VC_C7XKtNeeYOFuxTN3ee62wLS63nAcl4"
-      />
+      <head>
+        <meta
+          name="google-site-verification"
+          content="YJevxjbOP3qnYa2wMu3sQaDoy7cxLdH3cdTnDwbcqQI"
+        />
+        <meta
+          name="google-site-verification"
+          content="8SnxIePEB-VC_C7XKtNeeYOFuxTN3ee62wLS63nAcl4"
+        />
+        <script type="application/ld+json">
+          {JSON.stringify({
+            "@context": "https://schema.org/",
+            "@type": "Organization",
+            name: "Nodeflux",
+            description: "Indonesian Vision AI Company",
+            url: "https://www.nodeflux.io/",
+            logo: "https://www.nodeflux.io/logo.png",
+            sameAs: [
+              "https://twitter.com/nodefluxio",
+              "https://www.linkedin.com/company/nodeflux",
+            ],
+          })}
+        </script>
+      </head>
       <body className={inter.className}>
         <NavigationBar />
         {children}
